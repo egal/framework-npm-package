8e91efef82b3ba9bb90c1bb058f88e92af03208d
Yuliya <yushkova@sputnikfund.ru>
2021-08-10 19:33:18 +0700

    auth response receiving method updated

---
+++ compile/authTest.js
@@ -0,0 +1,28 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const AuthConstructor_1 = require("./src/Model/AuthConstructor");
+let newAuth = new AuthConstructor_1.EgalAuthConstructor({ modelName: 'User', userName: 'admin', password: 'password', url: 'http://breaker-develop.sputnikfund.ru/api', connectionType: 'axios' });
+let userData = { email: 'email134@email.com', password: 'password' };
+newAuth.registerNewUser(userData).then((data) => {
+    console.log('return from promise in test', data);
+    egalAuth();
+}).catch((error) => {
+    console.log(error, 'error from promise');
+});
+function egalAuth() {
+    console.log('egal auth called');
+    newAuth.authUser(userData).then((data) => {
+        egalLoginToService(data);
+        console.log(data, 'user login from test');
+    }).catch((error) => {
+        console.log(error, 'error from test');
+    });
+}
+function egalLoginToService(data) {
+    let loginData = { service_name: 'monolit', token: data[0] };
+    newAuth.loginToService(loginData).then((dataLoginToService) => {
+        console.log(dataLoginToService, 'login to service from test');
+    }).catch((error) => {
+        console.log(error, 'error from test');
+    });
+}
